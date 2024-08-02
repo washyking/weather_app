189436f9529fa110939b3144d5230fb8ad291ccb
washyking <maiblefive201@gmail.com>
2024-08-02 21:46:49 +0800

    pause

---
+++ src/components/Screens.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './Screens.css';
+
+
+function Screens() {
+  return (
+    <div className="Section_row">
+        <div className="box_row">
+            s
+        </div>
+        <div className="box_row">
+            s
+        </div>
+        <div className="box_row">
+            s
+        </div>
+        <div className="box_row">
+            s
+        </div>
+        
+    </div>
+  );
+}
+
+export default Screens;
---
+++ src/components/Title.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Title.css';
+
+
+function Title() {
+  return (
+    <div className="title_div">
+      <div className="title_content">
+        <h1>
+            Welcome to the Weather App
+        </h1>
+      </div>
+    </div>
+  );
+}
+
+export default Title;
